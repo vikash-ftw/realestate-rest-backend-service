81c20dcf79208f94798e5e27caad1aa68cb761e0
Vikash Batham <Vikash Batham@TARS>
2021-03-10 21:09:12 +0530

    landProperty ctrl skeleton

--- RealEstateBackEndRESTful/src/main/java/com/app/controllers/LandPropertyController.java
+++ RealEstateBackEndRESTful/src/main/java/com/app/controllers/LandPropertyController.java
@@ -1,10 +1,23 @@
 package com.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping()
-public class LandPropertyController {
+import com.app.dao.ILandProperty;
+import com.app.service.ILandPropertyService;
 
+@RestController
+@RequestMapping("/LandProperty")
+public class LandPropertyController {
+	
+	//D.I
+	@Autowired
+	private ILandPropertyService propertyService;
+	
+	public LandPropertyController() {
+		System.out.println("in ctrl of "+getClass().getName());
+	}
+	
+	
 }
